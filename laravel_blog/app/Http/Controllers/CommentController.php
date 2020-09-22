cffc6d66139267f930507274c1a308b4996d5c91
pragatisharma16 <sharmaprgti9@gmail.com>
2020-09-22 13:07:58 +0530

    laravel blog created

---
+++ laravel_blog/app/Http/Controllers/CommentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comments;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store(Request $request)
+  {
+    //on_post, from_user, body
+    $input['from_user'] = $request->user()->id;
+    $input['on_post'] = $request->input('on_post');
+    $input['body'] = $request->input('body');
+    $slug = $request->input('slug');
+    Comments::create($input);
+
+    return redirect($slug)->with('message', 'Comment published');
+  }
+
+  
+}
